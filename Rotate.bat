9c51a56aba565bd8dbce9a9179891637e49feafa
tao-shui <541441967@qq.com>
2024-09-14 00:10:13 -0700

    Add files via upload

--- Rotate.bat
+++ Rotate.bat
@@ -1,30 +1,20 @@
-@chcp 65001
+@chcp 65001 1>nul 2>nul
 @echo off
 
-if exist 1 (
-goto A
+if not exist 1 (
+md 1
 ) else (
+echo 已存在文件夹"1"
+echo 按任意键键覆盖并继续
+pause
 goto x1
 )
 
-:A
-echo 文件夹已存在,
-echo 注意会覆盖文件，
-echo 按任意键继续~！
-pause
-goto x2
-
 :x1
-md 1
 set /p s=输入旋转角度：
 for %%1 in (*.png *.jpg) do ffmpeg -i %%1 -vf "format=rgba,rotate=%s%*PI/180:ow=cos(%s%*PI/180)*iw+sin(%s%*PI/180)*ih:oh=sin(%s%*PI/180)*iw+cos(%s%*PI/180)*ih:c=none" -pix_fmt rgba -y "1/%%~n1.png"
 echo 搞定~！按任意键退出！
 pause
 exit
 
-:x2
-set /p s=输入旋转角度：
-for %%1 in (*.png *.jpg) do ffmpeg -i %%1 -vf "format=rgba,rotate=%s%*PI/180:ow=cos(%s%*PI/180)*iw+sin(%s%*PI/180)*ih:oh=sin(%s%*PI/180)*iw+cos(%s%*PI/180)*ih:c=none" -y "1/%%~n1.png"
-echo 搞定~！按任意键退出！
-pause
-exit
+
